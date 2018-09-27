36248a04c7d20ab8726225731eb22ab5b790171f
ryanliu285 <ryanliu8888@yahoo.com>
2018-09-26 18:53:27 -0700

    Another attempt that didn't work

--- Bot/Bot.js
+++ Bot/Bot.js
@@ -1,36 +1,32 @@
 var script = document.createElement('script');
 var script2 = document.createElement('script');
-script.src = 'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.8.0/xlsx.js';
-script2.src = 'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.8.0/jszip.js';
+script.src = 'https://cdnjs.cloudflare.com/ajax/libs/xls/0.7.4-a/xls.js';
 script.type = 'text/javascript';
-script2.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
-document.getElementsByTagName('head')[0].appendChild(script2);
 
-// function ExcelToJSON(file) {
-//
-//   this.parseExcel = function(file) {
-//     var reader = new FileReader();
-//
-//     reader.onload = function(e) {
-//       var data = e.target.result();
-//       var workbook = XLSX.read(data, {
-//         type: 'binary'
-//       });
-//
-//       workbook.SheetNames.forEach(function(sheetName) {
-//         var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
-//         var json_object = JSON.stringify(XL_row_object);
-//         console.log(json_object);
-//         return json_object;
-//       })
-//
-//     };
-//
-//     reader.onerror = function(ex) {
-//       console.log(ex);
-//     };
-//
-//     reader.readAsBinaryString(file);
-//   };
-// };
+function ExcelToJSON(file) {
+  // Get The File From The Input
+   var oFile = file;
+   var sFilename = file.name;
+   // Create A File Reader HTML5
+   var reader = new FileReader();
+
+   // Ready The Event For When A File Gets Selected
+   reader.onload = function(e) {
+       var data = e.target.result;
+       var cfb = XLS.CFB.read(data, {type: 'binary'});
+       var wb = XLS.parse_xlscfb(cfb);
+       // Loop Over Each Sheet
+       wb.SheetNames.forEach(function(sheetName) {
+           // Obtain The Current Row As CSV
+           var sCSV = XLS.utils.make_csv(wb.Sheets[sheetName]);
+           var oJS = XLS.utils.sheet_to_row_object_array(wb.Sheets[sheetName]);
+
+           $("#my_file_output").html(sCSV);
+           console.log(oJS)
+       });
+   };
+
+   // Tell JS To Start Reading The File.. You could delay this if desired
+   reader.readAsBinaryString(oFile);
+};
