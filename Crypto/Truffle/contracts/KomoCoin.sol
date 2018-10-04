c990044bd3ba3efcfe922595c1f1d0a81ea1aacf
ryanliu285 <ryanliu8888@yahoo.com>
2018-10-04 10:27:29 -0700

    Lots of Crypto stuff

---
+++ Crypto/Truffle/contracts/KomoCoin.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.17;
+
+import 'zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol';
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+
+contract GradientToken is ERC721Token, Ownable {
+  string public constant name = "KomoCoin";
+  string public constant symbol = "KC";
+}
