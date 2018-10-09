41dcab32ce60f6549ac98bda73ed1942105cddee
ryanliu285 <ryanliu8888@yahoo.com>
2018-10-09 11:46:36 -0700

    Gas amount still a problem

--- Crypto/crypto.sol
+++ Crypto/crypto.sol
@@ -66,36 +66,6 @@ contract ERC721 {
     function supportsInterface(bytes4 _interfaceID) external view returns (bool);
 }
 
-
-// // Auction wrapper functions
-
-
-// Auction wrapper functions
-
-
-
-
-
-
-
-/// @title SEKRETOOOO
-contract GeneScienceInterface {
-    /// @dev simply a boolean to indicate this is the contract we expect to be
-    function isGeneScience() public pure returns (bool);
-
-    /// @dev given genes of kitten 1 & 2, return a genetic combination - may have a random factor
-    /// @param genes1 genes of mom
-    /// @param genes2 genes of sire
-    /// @return the genes that are supposed to be passed down the child
-    function mixGenes(uint256 genes1, uint256 genes2, uint256 targetBlock) public returns (uint256);
-}
-
-
-
-
-
-
-
 /// @title A facet of KittyCore that manages special access privileges.
 /// @author Axiom Zen (https://www.axiomzen.co)
 /// @dev See the KittyCore contract documentation to understand how the various contract facets are arranged.
