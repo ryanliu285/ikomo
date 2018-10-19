dfb38686ddba5053a56df7aed3eea7e7a95a4c3e
Brandon <brandonh.lee617@gmail.com>
2018-10-19 10:03:23 -0700

    css changes

--- Home/includes/login.inc.php
+++ Home/includes/login.inc.php
@@ -5,21 +5,21 @@ if(isset($_POST['login-submit'])){
   require 'dbh.inc.php';
 
   $mailuid = $_POST['mailuid'];
-  $password = $_POSt['pwd'];
+  $password = $_POST['pwd'];
 
   if(empty($mailuid || empty($password))){
       header("Location: ../index.php?error=emptyfields");
       exit();
   }
   else {
-      $sql= "SELECT * FROM users WHERE uidUsers=? OR emailUsers =?;";
+      $sql= "SELECT * FROM users WHERE uidUsers=?;";
       $stmt = mysqli_stmt_init($conn);
       if (!mysqli_stmt_prepare($stmt, $sql)){
         header("Location: ../index.php?error=sqlerror");
         exit();
       }
       else {
-        mysqli_stmt_bind_param($stmt, "ss", $mailuid, $mailuid);
+        mysqli_stmt_bind_param($stmt, "s", $mailuid);
         mysqli_stmt_execute($stmt);
         $result = mysqli_stmt_get_result($stmt);
         if($row = mysqli_fetch_assoc($result)){
--- Home/signup.php
+++ Home/signup.php
@@ -9,6 +9,31 @@
      </div>
      <div class = "col-md-6">
        <center><h1>Signup</h1></center>
+       <?php
+          if(isset($_GET['error'])){
+            if($_GET['error'] == "emptyfields"){
+                echo '<center><p class = "signuperror">Fill in all fields!</p></center>';
+            }
+            else if ($_GET["error"] == "invalidmailuid"){
+              echo '<center><p class = "signuperror">Invalid Username and E-mail!</p></center>';
+            }
+            else if ($_GET["error"] == "invaliduid"){
+              echo '<center><p class = "signuperror">Invalid Username!</p></center>';
+            }
+            else if ($_GET["error"] == "invalidmail"){
+              echo '<center><p class = "signuperror">Invalid E-mail!</p></center>';
+            }
+            else if ($_GET["error"] == "passwordcheck"){
+              echo '<center><p class = "signuperror">Your passwords do not match!</p></center>';
+            }
+            else if ($_GET["error"] == "usertaken"){
+              echo '<center><p class = "signuperror">Username is already taken!!</p></center>';
+            }
+            else if ($_GET["signup" == "success"]){
+              echo '<center><p class = "signupsucc">Signup Successful!</p></center>';
+            }
+          }
+        ?>
        <form action = "includes/signup.inc.php" method = "post">
          <center><br><input type = "text" name = "uid" placeholder= "VEI Username"></br>
          <br><input type = "text" name = "mail" placeholder= "E-mail"></br>
