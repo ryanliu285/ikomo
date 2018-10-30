66a0cafa8d5ea3ddbcf2d31e5eda5f917768cd2c
Brandon <brandonh.lee617@gmail.com>
2018-10-29 20:29:38 -0700

    added stuff to homepage

--- Home/index.php
+++ Home/index.php
@@ -1,33 +1,27 @@
 <?php
-  require "header.php";
+ require "header.php";
  ?>
-
- <!DOCTYPE html>
+<!DOCTYPE html>
  <html>
   <head>
-      <link rel = "stylesheet" type = "text/css" href = "main.css"/>
-      <link rel = "stylesheet" href = "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin = "anonymous"/>
+    <link rel = "stylesheet" type = "text/css" href = "main.css"/>
+    <link rel = "stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
+    <title>VEI iKOMO</title>
   </head>
-  <body >
-    <div class = "container-fluid" id = "bg">
+  <body class = "bg">
+    <div class = "container-fluid">
       <div class = "row">
         <div class = "col-md-1">
 
         </div>
         <div class = "col-md-10">
-            <img src = "/img/sign-min.jpg"/>
             <p>CONTENT</p>
         </div>
         <div class = "col-md-1">
 
-        </dov>
+        </div>
       </div>
     </div>
   </body>
 
  </html>
-
-
- <?php
-  require "footer.php";
-  ?>
