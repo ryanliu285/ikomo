d8a85c4382f962f57ab729ce5da9f5c9d1038792
Brandon <brandonh.lee617@gmail.com>
2018-11-08 00:54:14 -0800

    fixed error handling

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -9,26 +9,32 @@ if(isset($_POST['signup-submit'])){
   $passwordRepeat = $_POST ['pwd-repeat'];
 
   if(empty($username) || empty($password) || empty($email) || empty($passwordRepeat)) {
-    header("Location: ../index.php?error=emptyfields&uid=".$username."&mail=".$email);
+    header("Location: ../signup1.php?error=emptyfields&uid=".$username."&mail=".$email);
+    exit();
   }
   else if (!filter_var($email, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z0-9\.]*$/", $username)) {
-    header("Location: ../index.php?error=invalidmailuid");
+    header("Location: ../signup1.php?error=invalidmailuid");
+    exit();
 }
   else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    header("Location: ../index.php?error=invalidmail&uid=".$username);
+    header("Location: ../signup1.php?error=invalidmail&uid=".$username);
+    exit();
 }
   else if (!preg_match("/^[a-zA-Z0-9\.]*$/", $username)) {
-    header("Location: ../index.php?error=invaliduid&mail=".$email);
+    header("Location: ../signup1.php?error=invaliduid&mail=".$email);
+    exit();
 }
   else if ($password !== $passwordRepeat) {
-    header("Location: ../index.php?error=passwordcheck&uid=".$username."&mail=".$email);
+    header("Location: ../signup1.php?error=passwordcheck&uid=".$username."&mail=".$email);
+    exit();
   }
   else {
 
     $sql = "SELECT uidUsers FROM iKOMODB WHERE uidUsers =?"; /* should be iKOMODB */
     $stmt = mysqli_stmt_init($conn);
     if(!mysqli_stmt_prepare($stmt, $sql)){
-      header("Location: ../signup.php?error=sqlerror");
+      header("Location: ../signup1.php?error=sqlerror");
+      exit();
     }
     else {
       mysqli_stmt_bind_param($stmt, "s", $username);
@@ -36,13 +42,15 @@ if(isset($_POST['signup-submit'])){
       mysqli_stmt_store_result($stmt);
       $resultCheck = mysqli_stmt_num_rows($stmt);
       if($resultCheck >0) {
-        header("Location: ../signup.php?error=usertaken&mail=".$email);
+        header("Location: ../signup1.php?error=usertaken&mail=".$email);
+        exit();
       }
       else {
         $sql = "INSERT INTO iKOMODB (uidUsers, emailUsers, pwdUsers) VALUES (?, ?, ?)";  /* should be iKOMODB */
         $stmt = mysqli_stmt_init($conn);
         if(!mysqli_stmt_prepare($stmt, $sql)){
-          header("Location: ../index.php?error=sqlerror");
+          header("Location: ../signup1.php?error=sqlerror");
+          exit();
         }
         else {
           $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
---
+++ signup1.php
@@ -0,0 +1,184 @@
+<?php
+require 'header.php';
+?>
+
+<!DOCTYPE html>
+ <html>
+  <head>
+    <link rel = "stylesheet" type = "text/css" href = "main.css"/>
+    <link rel = "stylesheet" type = "text/css" href = "animate.css"/>
+    <link rel = "stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
+    <style>
+      .signup {
+        text-align: center;
+      }
+      #overlay {
+        width: 100%;
+        height: 200%;
+        background-color: #F9F4D2;
+        position: fixed;
+        z-index: 101;
+      }
+      .percent-count {
+        width: 450px;
+        height: 50px;
+        margin: 10px auto;
+        font-size: 40px;
+        text-align: center;
+        color: #71DDE2;
+      }
+      .progress-bar {
+        width: 506px;
+        height: 26px;
+        background-color: #71DDE2;
+        border-radius: 13px;
+        margin: 10px auto;
+      }
+      .progress {
+        width: 25px;
+        height: 20px;
+        border-radius: 10px;
+        background-color: #34ABB7;
+      }
+      #loading {
+        height: 200px;
+        width: 100%;
+      }
+      #loadimage {
+        display: block;
+        margin: 10px auto;
+      }
+      #accountoverlay {
+        margin: auto;
+        width: 50%;
+        height: 500px;
+        z-index: 99;
+        background-color: rgba(235, 255, 253, 0.97);
+        display: block;
+        border-radius: 15px;
+        border: solid;
+        border-width: 10px;
+        border-color: rgba(136, 211, 229, 0.92);
+
+      }
+      #login {
+        float: left;
+        width: 100px;
+      }
+      .buttonx{
+        background-color: rgba(106, 196, 206, 0.92);
+        font-size: 18px;
+        font-family: "Dimbo";
+        text-decoration: none;
+        text-align: center;
+        border: none;
+        border-bottom-color: rgba(89, 154, 165, 0.92);
+        border-bottom-width: 2px;
+        border-bottom-style: solid;
+      }
+      .inputx {
+        background-color: rgba(0,0,0,0);
+        border: none;
+        border-bottom-width: 4px;
+        border-bottom-style: solid;
+        border-bottom-color: rgba(89, 154, 165, 0.92);
+        font-family: "Dimbo";
+        width: 250px;
+        font-size: 18px;
+      }
+      h2 {
+        font-family: "Dimbo";
+        font-size: 40px;
+      }
+      #footer {
+        font-family: "Dimbo";
+        font-size: 25px;
+        background-color: rgb(234, 254, 252);
+        border-top-color: rgb(89, 154, 165);
+        border-top-width: 5px;
+      }
+      #vei {
+        margin-top: 5px;
+        margin-bottom: 5px;
+      }
+      #disclaimer {
+        margin-top: 20px;
+      }
+      #signupoverlay {
+        margin: auto;
+        width: 50%;
+        height: 500px;
+        z-index: 99;
+        background-color: rgba(235, 255, 253, 0.97);
+        display: block;
+        border-radius: 15px;
+        border: solid;
+        border-width: 10px;
+        border-color: rgba(136, 211, 229, 0.92);
+      }
+    </style>
+    <title>VEI iKOMO</title>
+  </head>
+  <body class = "bg">
+    <div id = "signupoverlay">
+      <div class =  "container-fluid">
+          <div class = "row">
+              <div class = "signup col-md-6">
+                  <br>
+                  <img height = "100px" width = "auto" src = "./img/ikologo.png">
+              </div>
+              <div class = "signup col-md-6">
+              </div>
+          </div>
+          <div class = "row">
+              <div class = "col-md-3">
+              </div>
+              <div class = "col-md-6 signup">
+                <center><h2>Signup</h2></center>
+                <?php
+                   if(isset($_GET['error'])){
+                     if($_GET['error'] == "emptyfields"){
+                         echo '<center><p class = "signuperror">Fill in all fields!</p></center>';
+                     }
+                     else if ($_GET["error"] == "invalidmailuid"){
+                       echo '<center><p class = "signuperror">Invalid Username and E-mail!</p></center>';
+                     }
+                     else if ($_GET["error"] == "invaliduid"){
+                       echo '<center><p class = "signuperror">Invalid Username!</p></center>';
+                     }
+                     else if ($_GET["error"] == "invalidmail"){
+                       echo '<center><p class = "signuperror">Invalid E-mail!</p></center>';
+                     }
+                     else if ($_GET["error"] == "passwordcheck"){
+                       echo '<center><p class = "signuperror">Your passwords do not match!</p></center>';
+                     }
+                     else if ($_GET["error"] == "usertaken"){
+                       echo '<center><p class = "signuperror">Username is already taken!!</p></center>';
+                     }
+                     else if ($_GET["error"] == "success"){
+                       echo '<center><p class = "signupsucc">Signup Successful!</p></center>';
+                     }
+                   }
+                 ?>
+                <form action = "includes/signup.inc.php" method = "post">
+                  <center><input class = "inputx" type = "text" name = "uid" placeholder= "VEI Username">
+                  <br>
+                  <input class = "inputx" type = "text" name = "mail" placeholder= "E-mail">
+                  <br>
+                  <input class = "inputx" type = "password" name = "pwd" placeholder= "Password">
+                  <br>
+                  <input class = "inputx" type = "password" name = "pwd-repeat" placeholder= "Repeat Password">
+                  <br>
+                  <button class = "buttonx" type = "submit" name = "signup-submit">Finish Sign Up!</button>
+                </center>
+                </form>
+              </div>
+              <div class = "col-md-3">
+              </div>
+              </div>
+          </div>
+      </div>
+  </body>
+
+
+ </html>
