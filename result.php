b7454e588aad492b356ff06fcb943d3b99aa5789
ryanliu285 <ryanliu8888@yahoo.com>
2018-11-16 10:28:41 -0800

    added new page and fixed result images

--- result.php
+++ result.php
@@ -7,6 +7,8 @@ $animal = preg_replace('/\s+/', '', $animal);
 $url = strtolower($animal) . ".png";
 if($rarity === "rare"){
 echo "<br><img style = 'background-color: rgb(68, 181, 200);
+height: 300px;
+width: auto;
 border-radius: 15px;
 border-color: rgb(38, 152, 171);
 border-width: 10px;
@@ -15,6 +17,8 @@ border-style: solid;' src = './img/ikomos/$url' alt = 'Sorry! iKOMO images comin
 if($rarity === "common"){
   echo "<br><img style = 'background-color: rgb(193, 200, 161);
   border-radius: 15px;
+  height: 300px;
+  width: auto;
   border-color: rgb(171, 185, 110);
   border-width: 10px;
   border-style: solid;' src = './img/ikomos/$url' alt = 'Sorry! iKOMO images coming soon!'>";
@@ -22,6 +26,8 @@ if($rarity === "common"){
 if($rarity === "epic"){
   echo "<br><img style = 'background-color: rgb(211, 146, 224);
   border-radius: 15px;
+  height: 300px;
+  width: auto;
   border-color: rgb(191, 60, 217);
   border-width: 10px;
   border-style: solid' src = './img/ikomos/$url' alt = 'Sorry! iKOMO images coming soon!'>";
@@ -29,6 +35,8 @@ if($rarity === "epic"){
 if($rarity === "legendary"){
   echo "<br><img style = 'background-color: rgb(215, 189, 74);
   border-radius: 15px;
+  height: 300px;
+  width: auto;
   border-color: rgb(198, 164, 18);
   border-width: 10px;
   border-style: solid;' src = './img/ikomos/$url' alt = 'Sorry! iKOMO images coming soon!'>";
