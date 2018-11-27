d741efbade1c420afdf7b258f54523277ae946ed
Brandon <brandonh.lee617@gmail.com>
2018-11-27 11:10:56 -0800

    more resized ikomos

--- mainInterface.php
+++ mainInterface.php
@@ -22,6 +22,184 @@
     #disclaimtxt{
       color: white;
     }
+    .signup {
+      text-align: center;
+    }
+    #overlay {
+      width: 100%;
+      height: 200%;
+      background-color: #F9F4D2;
+      position: fixed;
+      z-index: 101;
+    }
+    .percent-count {
+      width: 450px;
+      height: 50px;
+      margin: 10px auto;
+      font-size: 40px;
+      text-align: center;
+      color: #71DDE2;
+    }
+    .progress-bar {
+      width: 506px;
+      height: 26px;
+      background-color: #71DDE2;
+      border-radius: 13px;
+      margin: 10px auto;
+    }
+    .progress {
+      width: 25px;
+      height: 20px;
+      border-radius: 10px;
+      background-color: #34ABB7;
+    }
+    #loading {
+      height: 200px;
+      width: 100%;
+    }
+    #loadimage {
+      display: block;
+      margin: 10px auto;
+    }
+    #accountoverlay {
+      margin: auto;
+      width: 50%;
+      height: 500px;
+      z-index: 99;
+      background-color: rgba(235, 255, 253, 0.97);
+      display: block;
+      border-radius: 15px;
+      border: solid;
+      border-width: 10px;
+      border-color: rgba(136, 211, 229, 0.92);
+
+    }
+    #login {
+      float: left;
+      width: 100px;
+    }
+    .buttonx{
+      background-color: rgba(106, 196, 206, 0.92);
+      font-size: 18px;
+      font-family: "Dimbo";
+      text-decoration: none;
+      text-align: center;
+      border: none;
+      border-bottom-color: rgba(89, 154, 165, 0.92);
+      border-bottom-width: 3px;
+      border-bottom-style: solid;
+    }
+    .buttonx:hover {
+      cursor: pointer;
+    }
+    .inputx {
+      background-color: rgba(0,0,0,0);
+      border: none;
+      border-bottom-width: 4px;
+      border-bottom-style: solid;
+      border-bottom-color: rgba(89, 154, 165, 0.92);
+      font-family: "Dimbo";
+      width: 250px;
+      font-size: 18px;
+    }
+    h2 {
+      font-family: "Dimbo";
+      font-size: 50px;
+    }
+    #footer {
+      font-family: "Dimbo";
+      font-size: 25px;
+      background-color: rgb(234, 254, 252);
+      border-top-color: rgb(89, 154, 165);
+      border-top-width: 5px;
+    }
+    #signupoverlay {
+      margin: auto;
+      width: 50%;
+      height: 550px;
+      z-index: 99;
+      background-color: rgba(235, 255, 253, 0.97);
+      display: none;
+      border-radius: 15px;
+      border: solid;
+      border-width: 10px;
+      border-color: rgba(136, 211, 229, 0.92);
+    }
+    .signuperror {
+      color: rgb(210, 22, 25);
+      font-family: "Dimbo";
+      font-size: 16px;
+    }
+    .signupsucc {
+      color: rgb(22, 152, 47);
+      font-family: "Dimbo";
+      font-size: 16px;
+    }
+    #aboutanim {
+      padding: 3px;
+    }
+    #signupanim {
+      padding: 3px;
+    }
+
+    #returnanim{
+      padding: 4px;
+    }
+    #footer {
+      position: fixed;
+      left: 0;
+      bottom: 0;
+      width: 100%;
+      height: 75px;
+      background-color: rgba(0, 0, 0, 0.5);
+    }
+    #closedisclaimer{
+      color: white;
+    }
+    #closedisclaimer:hover{
+      cursor: pointer;
+    }
+    #disclaimtxt{
+      color: white;
+    }
+    .signtxt{
+      font-size: 14px;
+    }
+    #mkp{
+      margin-top: 145%;
+      float: right;
+    }
+    #arc{
+      float: left;
+      margin-top: 145%;
+    }
+    #hme {
+      margin-top: 105%;
+      float: left;
+      position: relative;
+      right: 45%;
+    }
+    #str{
+      margin-top: 105%;
+      float: right;
+      position: relative;
+      left: 39%;
+      z-index: 99;
+    }
+    .bigbt{
+      background-color: #F7E188;
+      font-size: 18px;
+      font-family: "Dimbo";
+      text-decoration: none;
+      text-align: center;
+      border: none;
+      border-bottom-color:  #daba38;
+      border-bottom-width: 3px;
+      border-bottom-style: solid;
+    }
+    #loggedin {
+      display: none;
+    }
     </style>
     <link rel = "stylesheet" type = "text/css" href = "main.css"/>
     <link rel = "stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
@@ -37,8 +215,8 @@
         <div class = "col-md-1">
         </div>
         <div id = "mainWelcomeText" class = "col-md-10">
-            <a onclick="window.location.href = './buycoins.php'"><button id = "buyButton">Buy KOMOcoins!</button></a>
-            <a onclick="balancecheck()"><button id = "buyButton2">Buy iKOMO!</button></a>
+            <a onclick="window.location.href = './buycoins.php'"><button class = "bigbt" id = "buyButton">Buy KOMOcoins!</button></a>
+            <a onclick="balancecheck()"><button class = "bigbt" id = "buyButton2">Buy iKOMO!</button></a>
             <script>
             var x;
             function logincheck(){
