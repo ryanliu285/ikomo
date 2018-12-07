f71eb36ffc048930c17ecb7d5cdcee88d07442c5
Brandon <brandonh.lee617@gmail.com>
2018-12-07 09:01:00 -0800

    added phpmailer contact works now

--- contact.php
+++ contact.php
@@ -1,5 +1,36 @@
 <?php
 require 'header.php';
+use PHPMailer\PHPMailer\PHPMailer;
+include_once "PHPMailer/PHPMailer.php";
+include_once "PHPMailer/Exception.php";
+include_once "PHPMailer/SMTP.php";
+if(isset($_POST['mail-submit'])) {
+  $subject = 'VEI iKOMO Support';
+  $email = $_POST['email'];
+  $user = $_POST['user'];
+  $message = $_POST['message'];
+  $mail = new PHPMailer();
+
+  $mail->Host = "smtp.gmail.com";
+  $mail->Port = 587; //587
+  $mail->SMTPAuth = true;
+  $mail->SMTPSecure = "tls"; //TLS
+  $mail->Username = "brandonh.lee617@gmail.com"; //ikomo.vei@gmail.com
+  $mail->Password = "Brand0nl33"; //cryptokingdom19
+
+  $mail->setFrom($email);
+  $mail->addAddress('ikomo.vei@gmail.com');
+  $mail->isHTML(true);
+  $mail->Subject = $subject;
+  $mail->Body = $message;
+
+  if($mail->send()){
+    $msg = "1";
+  }
+  else {
+    $msg = "2";
+  }
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -66,6 +97,16 @@ require 'header.php';
 .buttonx:hover {
   cursor: pointer;
 }
+.success {
+  color: rgb(22, 187, 25);
+  font-size: 24px;
+  font-family: ("Dimbo");
+}
+.failed {
+  color: rgb(175, 18, 47);
+  font-size: 24px;
+  font-family: ("Dimbo");
+}
   </style>
   </head>
 <body class = "contactbg">
@@ -77,14 +118,25 @@ require 'header.php';
     <div id = "contactoverlay" class = "col-md-6">
       <center>
          <h2>Contact Us!</h2>
-                    <form class = "contactform" action = "includes/maillog.inc.php" method = "post">
-                    <input class = "inputx input" type = "text" name = "Email" placeholder = "Email" required>
+         <?php if ($msg == 1){
+           echo '<p class = "success">Email Sent!</p>';
+         } else {
+           if($msg ==2){
+           echo '<p class = "failed">Invalid Email Address.</p>';
+           }
+           else {
+
+           }
+         }
+          ?>
+                    <form class = "contactform" action = "contact.php" method = "post">
+                    <input class = "inputx input" type = "text" name = "email" placeholder = "Email" required>
                     <br>
                     <br>
-                    <input class = "inputx input" type = "text" name = "User" placeholder = "Username" required>
+                    <input class = "inputx input" type = "text" name = "user" placeholder = "Username" required>
                     <br>
                     <br>
-                    <textarea class = "message2 inputx" type = "text" name = "Message" placeholder = "Message" required></textarea>
+                    <textarea class = "message2 inputx" type = "text" name = "message" placeholder = "Message" required></textarea>
                     <br>
                     <br>
                     <button class = "buttonx" type = "submit" name = "mail-submit">Send Email.</button></form>';
